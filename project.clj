17d2b5921ad917506c4cab5bfb5a24703b3e3ef4
Adel Khamatov <adelkhamatov@yandex.ru>
2020-09-16 13:41:39 +0300

    Try handle post and get \patients

--- project.clj
+++ project.clj
@@ -3,13 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [
-                 [org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-defaults "0.3.2"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-jetty-adapter "1.8.1"]
                  [org.clojure/java.jdbc "0.7.11"]
-                 [org.postgresql/postgresql "42.2.16.jre7"]]
+                 [org.postgresql/postgresql "42.2.16.jre7"]
+                 [ring/ring-json "0.5.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler medical-records.server/app :nrepl {:start? true}}
   :main ^:skip-aot medical-records.server
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]]}})
--- src/medical_records/server.clj
+++ src/medical_records/server.clj
@@ -1,16 +1,29 @@
 (ns medical-records.server
-  (:require [ring.adapter.jetty :as jetty])
-  (:require [compojure.core :refer [defroutes POST GET]]))
+  (:require [compojure.handler :as handler]
+            [compojure.core :refer [defroutes POST GET]]
+            [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [clojure.java.jdbc :as sql])
+  (:gen-class))
 
-(defroutes routes
-  (GET "/" [] "Hello world")
-  (POST "/patients" [request] "created"))
+(def spec "postgresql://localhost:5432/medical-records")
 
-(jetty/run-jetty
- #'routes
- {:host "127.0.0.1"
-  :port 8081
-  :join? false})
+(defn get-all-patients []
+  (sql/query spec ["select * from patients order by id"]))
 
-(defn -main []
-  (println "Server running at http://127.0.0.1:8081/"))
+(defn create-patient [patient]
+  (sql/insert! spec :patients 
+               [:name :address] 
+               [(get-in patient ["name"])
+                (get-in patient ["address"])]))
+
+(defroutes app-routes
+  (GET "/patients" [] (get-all-patients))
+  (POST "/patients" {body :body} (create-patient body))
+  (GET "/" [] "Hello world!")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (handler/api app-routes)
+      (wrap-json-body)
+      (wrap-json-response)))
